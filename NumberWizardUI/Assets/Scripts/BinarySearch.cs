44c2c2b62503446e502de184c024c3d5c127077c
Tibor Düsüngülü <dusungulu.tibi@gmail.com>
2020-08-23 17:39:24 +0200

    number wizard

---
+++ NumberWizardUI/Assets/Scripts/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class BinarySearch : MonoBehaviour
+{
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    private int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+}
---
+++ NumberWizardUI/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        // get active scene
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // load nex scene
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        // quit game when it's built
+        Application.Quit();
+    }
+}
